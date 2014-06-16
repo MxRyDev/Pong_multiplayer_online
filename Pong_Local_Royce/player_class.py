0e6d8533ec5faae7cfd849ecf14915cb123c20dd
Royce Pope <roycepope@gmail.com>
2014-06-15 17:27:15 -0700

    Initial Upload of Royce Local Test
    From Max Local version

---
+++ Pong_Local_Royce/player_class.py
@@ -0,0 +1,50 @@
+# Defines the player class to be imported into localpong_main
+
+if __name__ == '__main__':
+    print('''
+    This file is used to define the PLAYER class for Local Pong.
+    it is to be used within "localpong_main.py".
+    on its own, it provides no functionality.
+    
+    HAFF A GUD DAY
+    ''')
+    
+import pygame.sprite
+from constants import *
+
+
+
+# PLAYER CLASS
+class Player(pygame.sprite.Sprite):
+    
+    def __init__(self, startx, image):
+        # init position variables
+        self.change_x = 0
+        self.change_y = 0
+        # inherited class init
+        pygame.sprite.Sprite.__init__(self)
+        
+        # setup image
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = startx
+        self.rect.y = (SCREEN_HEIGHT/2 - PLAYER_HEIGHT/2)
+        
+        # add to lists
+        all_sprites_list.add(self)
+        player_list.add(self)
+        
+    # Define movement methods:
+    def changespeed(self, y):
+        self.change_y += y
+        
+    def move(self):
+        
+        # move
+        self.rect.y += self.change_y
+        
+        # keep on screen:
+        if self.rect.bottom > SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
--- Resources/pygame_first.py
+++ Resources/pygame_first.py
@@ -2,13 +2,17 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((700, 400))
+screen = pygame.display.set_mode((800, 500))
 pygame.display.set_caption('Hello World')
 
+# Colors
+RED = (255, 0, 0)
 
 # --- init Variables ---
 clock = pygame.time.Clock()
 pygame.mouse.set_visible(False)
+wall_1 = pygame.Rect(10, 20, 200, 500)
+pygame.draw.rect(screen, RED, (200, 150, 100, 50))
 
 while True: # main game loop
     for event in pygame.event.get():
