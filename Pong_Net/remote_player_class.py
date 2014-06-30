0dd3b2c0c1d70acd15b632cc174a47d976a52cd5
Royce Pope <roycepope@gmail.com>
2014-06-29 21:35:19 -0700

    messup

--- Pong_Net/remote_player_class.py
+++ Pong_Net/remote_player_class.py
@@ -20,9 +20,10 @@ class Remote_player():
         listen_thread.start()
         
     def listen(self):
-        data = s.recv(1024)
-        XandY = pickle.loads(data)
-        self.x, self.y = XandY[0], XandY[1]
+        while True:
+            data = s.recv(1024)
+            XandY = pickle.loads(data)
+            self.x, self.y = XandY[0], XandY[1]
     # grab 2 item list and update self.x and self.y constantly
     
     def speak(self, posX, posY):
